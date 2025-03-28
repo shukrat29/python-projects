35fb3b343d2e78dcc5f5425887caad6d2dfbdb71
Shukra Tamang <shukratamang.npl@gmail.com>
2025-03-28 11:52:18 +1100

    add two numbers using function

---
+++ 0.python-basics/function/addition.py
@@ -0,0 +1,11 @@
+def add_numbers(a,b):
+    return a + b
+
+
+# Taking input from the user
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+# Calling the function
+result = add_numbers(num1, num2)
+print("Sum:", result)

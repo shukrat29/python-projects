3bf086aad1355961d4d05b466628c0394e089dbd
Shukra Tamang <shukratamang.npl@gmail.com>
2025-03-28 12:23:31 +1100

    variables

---
+++ python-basics/variables/main.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+favourate_color = input("What is your favorite color? ")
+
+print( name + " likes " + favourate_color + " color ")
+
+
+
+dob = input("Enter your birth year: ")
+age = 2025 - int(dob)
+print(" You are " + str(age) + " years old ")
